12d1e4a4ad748e466dc6058a595cb54a3a402106
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@87886287-3c3c-bf74-c0a6-4f14d648827e>
2012-11-23 21:16:27 +0000

    RISTRUTTURAZIONE 1

---
+++ trunk/Azienda/src/it/util/format/StringFormat.java
@@ -0,0 +1,12 @@
+package it.util.format;
+
+public class StringFormat {
+	/**
+	 * @param metodo
+	 * @param message
+	 * @return il messaggio nel formato per il log
+	 */
+	public static String formatMessage(String metodo, String message){
+		return new StringBuilder("[").append(metodo).append("][").append(message).append("]").toString();
+	}
+}
