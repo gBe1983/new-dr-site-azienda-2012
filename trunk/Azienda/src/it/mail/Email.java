8649163361492e539b1f06d91f704d54a0009fe4
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@87886287-3c3c-bf74-c0a6-4f14d648827e>
2012-12-29 12:10:13 +0000

    Centralizzazzione pulizzia sessione Centralizzata gestione mail Aggiunti parametri mail e urlSite al web.xml

--- trunk/Azienda/src/it/mail/Email.java
+++ trunk/Azienda/src/it/mail/Email.java
@@ -12,49 +12,61 @@ import javax.mail.Transport;
 import javax.mail.URLName;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.ServletContext;
 
 public class Email{
 	private MyLogger log;
+	private String protocol = null;
+	private String host = null;
+	private String port = null;
+	private String folder = null;
+	private String user = null;
+	private String pass = null;
+	private String debug = null;
+	private String smtpAuth = null;
+	private String startTls = null;
+	private String socketFactoryClass = null;
 
-	public Email() {
+	public Email(ServletContext servletContext) {
 		log = new MyLogger(this.getClass());
+		final String metodo = "costruttore";
+		log.start(metodo);
+		protocol = servletContext.getInitParameter("mail.protocol");
+		host = servletContext.getInitParameter("mail.host");
+		port = servletContext.getInitParameter("mail.port");
+		user = servletContext.getInitParameter("mail.userName");
+		pass = servletContext.getInitParameter("mail.password");
+		folder = servletContext.getInitParameter("mail.folder");
+		debug = servletContext.getInitParameter("mail.debug");
+		smtpAuth = servletContext.getInitParameter("mail.smtp.auth");
+		startTls = servletContext.getInitParameter("mail.smtp.starttls.enable");
+		socketFactoryClass = servletContext.getInitParameter("mail.smtp.socketFactory.class");
+		log.end(metodo);
 	}
 
-	private final static String host = "smtp.gmail.com";
-	private final static String user = "info.drconsulting@gmail.com";
-	private final static String pass = "R0bert01";
-
-	public void sendMail(String dest, String mitt, String oggetto, String testoEmail){
+	public void sendMail(String dest, String oggetto, String testoEmail){
 		final String metodo = "sendMail";
 		log.start(metodo);
 		try {
 			Properties props = System.getProperties();
-			// Setup mail servera
 			props.put("mail.smtp.host", host);
-			props.put("mail.debug", "true");
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.socketFactory.port", "465");
-			props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			props.put("mail.smtp.port", "465");
-			// Session
+			props.put("mail.debug", debug);
+			props.put("mail.smtp.auth", smtpAuth);
+			props.put("mail.smtp.starttls.enable", startTls);
+			props.put("mail.smtp.socketFactory.port", port);
+			props.put("mail.smtp.socketFactory.class", socketFactoryClass);
+			props.put("mail.smtp.port", port);
 			Session session = Session.getDefaultInstance(props, null);
-			session.setDebug(true);
-			session.setPasswordAuthentication(new URLName("smtp", host, 465, "INBOX", user, pass), new PasswordAuthentication(user, pass));
-
-			// Inizializzo il messaggio
+			session.setPasswordAuthentication(
+				new URLName(protocol, host, Integer.parseInt(port), folder, user, pass),
+				new PasswordAuthentication(user, pass));
 			MimeMessage msg = new MimeMessage(session);
-			// Setto l'indirizzo del mittente
-			msg.setFrom(new InternetAddress(mitt));
-			// Setto gli indirizzi (destinatario e ccn)
+			msg.setFrom(new InternetAddress(user));
 			msg.addRecipient(Message.RecipientType.TO, new InternetAddress(dest));
-			msg.addRecipient(Message.RecipientType.BCC, new InternetAddress( mitt));
-			// Setto l'oggetto
+			msg.addRecipient(Message.RecipientType.BCC, new InternetAddress( user));
 			msg.setSubject(oggetto);
-			// Setto il corpo del testo
 			msg.setContent(testoEmail, "text/html");
-			// Invio messaggio
-			Transport tr = session.getTransport("smtp");
+			Transport tr = session.getTransport(protocol);
 			tr.connect(host, user, pass);
 			msg.saveChanges();
 			tr.sendMessage(msg, msg.getAllRecipients());
