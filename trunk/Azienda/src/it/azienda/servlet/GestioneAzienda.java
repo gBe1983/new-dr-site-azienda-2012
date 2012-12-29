e18a25447cfd384aeb3dea36548d182baac2f394
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@87886287-3c3c-bf74-c0a6-4f14d648827e>
2012-12-29 12:08:50 +0000

    Centralizzazzione pulizzia sessione Centralizzata gestione mail Aggiunti parametri mail e urlSite al web.xml

--- trunk/Azienda/src/it/azienda/servlet/GestioneAzienda.java
+++ trunk/Azienda/src/it/azienda/servlet/GestioneAzienda.java
@@ -4,15 +4,12 @@ import it.azienda.dao.AziendaDAO;
 import it.azienda.dao.UtenteDAO;
 import it.azienda.dto.AziendaDTO;
 import it.azienda.dto.UtenteDTO;
-import it.mail.Email;
+import it.util.log.MyLogger;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Enumeration;
+import java.util.Calendar;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -23,62 +20,42 @@ import javax.servlet.http.HttpSession;
  * Servlet implementation class GestioneAzienda
  */
 public class GestioneAzienda extends BaseServlet{
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -7318371187106812683L;
+	private MyLogger log;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		final String metodo="doGet";
+		log.start(metodo);
 		processRequest(request,response);
+		log.end(metodo);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		final String metodo="doGet";
+		log.start(metodo);
 		processRequest(request,response);
+		log.end(metodo);
 	}
-	
+
 	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		final String metodo="processRequest";
+		log.start(metodo);
 		HttpSession sessione = request.getSession();
-		
 		String messaggio = "";
-		
-		//questa classe mi serve per la gestione dei passaggi dati tra jsp e servlet
-		RequestDispatcher rd = null;
-		
-		//creo l'istanza della classe UtenteDAO
+
 		UtenteDAO uDAO = new UtenteDAO(conn.getConnection());
-		
-		//creo l'istanza della classe AziendaDAO
 		AziendaDAO aDAO = new AziendaDAO(conn.getConnection());
-		
-		//creo l'istanza della classe Connessione
-//		Connessione connessione = new Connessione();
-		
-//		Connection conn = null;
-//		
-//		//recupero la Connection
-//		if(request.getParameter("connessione") != null){
-//			conn = connessione.connessione(request.getParameter("connessione"));
-//			sessione.setAttribute("modalitaDiConnessione", request.getParameter("connessione"));
-//		}else{
-//			conn = (Connection) sessione.getAttribute("connessione");
-//		}
-//		
-		//setto in sessione la connessione
+
 		sessione.setAttribute("connessione", conn.getConnection());//TODO VERIFICARE COME TOGLIERE IL PASSAGGIO DELLA CONNESSIONE ALLE 3 JSP CHE LA SFRUTTANO
-		
-		//recupero il valore che può assumere l'attributo "azione"
+
 		String azione = request.getParameter("azione");
-		
 		if(azione.equals("registrazioneAzienda") || azione.equals("modificaAzienda")){
-			UtenteDTO utente = new UtenteDTO();
-			//recupero tutti i dati relativi all'inserimento dell'azienda
 			AziendaDTO azienda = new AziendaDTO();
 			azienda.setRagioneSociale(request.getParameter("ragioneSociale"));
 			azienda.setIndirizzo(request.getParameter("indirizzo"));
@@ -100,10 +77,10 @@ public class GestioneAzienda extends BaseServlet{
 			azienda.setTelefonoReferente(request.getParameter("telefonoReferente"));
 			azienda.setSito(request.getParameter("sito"));
 			azienda.setTrattamentoDati(true);
-			
+
 			if(azione.equals("registrazioneAzienda")){
 				messaggio = aDAO.inserimentoAzienda(azienda);
-				
+				UtenteDTO utente = new UtenteDTO();
 				if(messaggio.equals("")){
 					messaggio = "Siamo spiacenti, per via dei problemi tecnici la registrazione non avvenuta con successo. Contattare l'amministrazione";
 				}else if(messaggio.equals("Registrazione avvenuta con successo")){
@@ -111,26 +88,22 @@ public class GestioneAzienda extends BaseServlet{
 					utente.setUsername(request.getParameter("username"));
 					utente.setPassword(request.getParameter("password"));
 					utente.setId_azienda(aDAO.caricamentoIdAzienda());
-					utente.setData_registrazione((new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date())));
-					utente.setData_login((new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(new Date())));
-					
+					String now = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss").format(Calendar.getInstance().getTime());
+					utente.setData_registrazione(now);
+					utente.setData_login(now);
 					uDAO.inserimentoUtente(utente);
 				}
-				String testoEmail="<html>" +  "<head>" +
-			       " <title> Registrazione Account </title>" +
-			       "</head>" +  "<body>" +
-			       "Gentile cliente " + azienda.getRagioneSociale()+ ",<br>" +
-			       "<p> grazie di esserti registrato presso di noi." + "<br><br> I dati d'accesso sono:" + "<br>" + 
-			       "username: " + utente.getUsername() + "<br>"+ 
-			       "password: " + utente.getPassword() + "<br>"+
-			       "<br><br> Presto sarai abilitato all'accesso sul nostro portale. </p><br/> Cordiali Saluti <br><br><br> Lo Staff</p>" +
-			       "</body>" +
-			       "</html>";
+				StringBuilder testoEmail= new StringBuilder("<html><head><title> Registrazione Account </title></head><body>Gentile cliente ");
+				testoEmail	.append(azienda.getRagioneSociale())
+							.append(",<br><p> grazie di esserti registrato presso di noi.<br><br> I dati d'accesso sono:<br>username: ")
+							.append(utente.getUsername())
+							.append("<br>password: ")
+							.append(utente.getPassword())
+							.append("<br><br><br> Presto sarai abilitato all'accesso sul nostro portale. </p><br/> Cordiali Saluti <br><br><br> Lo Staff</p></body></html>");
 				try {
-					new Email().sendMail(azienda.getMail(), "info.drconsulting@gmail.com", "Iscrizione Nuovo Utente Aziendale", testoEmail);
+					mail.sendMail(azienda.getMail(), "Iscrizione Nuovo Utente Aziendale", testoEmail.toString());
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					log.error(metodo, "invio mail creazione utente aziendale fallita", e);
 				}
 			}else{
 				
@@ -142,120 +115,41 @@ public class GestioneAzienda extends BaseServlet{
 				}
 			}
 			request.setAttribute("messaggio", messaggio);
-			rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio");
-			rd.forward(request, response);
-		
+			getServletContext()
+				.getRequestDispatcher("/index.jsp?azione=messaggio")
+					.forward(request, response);
 		}else if(azione.equals("login")){
-			
 			int idAzienda = Integer.parseInt(request.getParameter("utente"));
-			
 			UtenteDTO utenteLoggato = uDAO.caricamentoAzienda(idAzienda);
-			
 			sessione.setAttribute("utenteLoggato", utenteLoggato);
 			response.sendRedirect("./index.jsp?azione=homePage");
-			
 		}
-		
-	if(sessione.getAttribute("utenteLoggato") != null){
-		
-		if(azione.equals("visualizzaAzienda") || azione.equals("aggiornaAzienda")){
-			
-			/*
-			 * in questa sezione effettuo il caricamento del profilo dell'Azienda
-			 */
-			AziendaDTO azienda = aDAO.visualizzaProfiloAzienda(((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
-			
-			request.setAttribute("profiloAzienda", azienda);
-			
-			if(azione.equals("visualizzaAzienda")){
-				rd = getServletContext().getRequestDispatcher("/index.jsp?azione=visualizzaAzienda");
-				rd.forward(request, response);
-			}else{
-				rd = getServletContext().getRequestDispatcher("/index.jsp?azione=aggiornaAzienda");
-				rd.forward(request, response);
+		if(sessione.getAttribute("utenteLoggato") != null){
+			if(azione.equals("visualizzaAzienda") || azione.equals("aggiornaAzienda")){//in questa sezione effettuo il caricamento del profilo dell'Azienda
+				AziendaDTO azienda = aDAO.visualizzaProfiloAzienda(((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
+				request.setAttribute("profiloAzienda", azienda);
+				getServletContext()
+				.getRequestDispatcher("/index.jsp?azione="+azione)
+					.forward(request, response);
+			}else if(azione.equals("logout")){
+				clearSession(sessione);
+				response.sendRedirect(prop.getProperty("siteUrl"));
+			}else if(azione.equals("eliminaProfilo")){//in questa sezione effettuo l'eliminazione logica dell'azienda
+				aDAO.eliminaProfiloAzienda(((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
+				clearSession(sessione);
+				response.sendRedirect("./index.jsp?azione=homePage");
+			}else if(azione.equals("cambioPassword")){//in questa sezione effettuo l'eliminazione logica dell'azienda
+				String messaggioCambioPass = aDAO.cambioPassword(request.getParameter("nuovaPassword"),((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
+				if(messaggioCambioPass.equals("ok")){
+					request.setAttribute("messaggio", "Cambio della password avvenuta con successo");
+					getServletContext()
+						.getRequestDispatcher("/index.jsp?azione=messaggio")
+							.forward(request, response);
+				}
 			}
-			
-		}else if(azione.equals("logout")){
-			
-			Enumeration attrNames = sessione.getAttributeNames();
-			while (attrNames.hasMoreElements())	{		  
-					String valoriSessione = (String) attrNames.nextElement();// Stampa i nomi di tutti gli attributi di sessione
-					sessione.removeAttribute(valoriSessione);
-					System.out.println(valoriSessione);
-
-			}
-			
-			//TODO ricordarsi di cambiare ogni volta per i due ambienti
-			//response.sendRedirect("http://drconsulting.tv");
-			response.sendRedirect("http://cvonline.tv");
-		
-		}else if(azione.equals("eliminaProfilo")){
-			/*
-			 * in questa sezione effettuo l'eliminazione logica dell'azienda
-			 */
-			
-			aDAO.eliminaProfiloAzienda(((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
-			
-			Enumeration enumerati = sessione.getAttributeNames();
-			while (enumerati.hasMoreElements())	{
-				String nomeOggetto = (String) enumerati.nextElement();
-				System.out.println(nomeOggetto);// Stampa i nomi di tutti gli attributi di sessione
-				sessione.removeAttribute(nomeOggetto);
-			}
-			
-			response.sendRedirect("./index.jsp?azione=homePage");
-			
-			
-		}else if(azione.equals("cambioPassword")){
-			/*
-			 * in questa sezione effettuo l'eliminazione logica dell'azienda
-			 */
-			
-			String messaggioCambioPass = aDAO.cambioPassword(request.getParameter("nuovaPassword"),((UtenteDTO)sessione.getAttribute("utenteLoggato")).getId_azienda());
-			if(messaggioCambioPass.equals("ok")){
-				request.setAttribute("messaggio", "Cambio della password avvenuta con successo");
-				rd = getServletContext().getRequestDispatcher("/index.jsp?azione=messaggio");
-				rd.forward(request, response);
-			}
-			
+		}else{
+			sessioneScaduta(response);
 		}
-	 }else{
-		 	response.setContentType("text/html");
-	        PrintWriter out = response.getWriter();
-			try {
-				out = response.getWriter();
-				out.print("<html>" +
-						"<head>" +
-						"</head>" +
-						"<body>" +
-						"<script type=\"text/javascript\">" +
-						"alert(\"La sessione è scaduta. Rieffettuare la login\");" +
-						"url = window.location.href;" +
-						"var variabiliUrl = url.split(\"/\");" +
-						"for(a=0; a < variabiliUrl.length; a++){" +
-						"		if(a == 2){" +
-						"			var localVariabili = variabiliUrl[a].split(\":\");" +
-						"			for(x=0; x < localVariabili.length; x++){" +
-						"				if(localVariabili[x] == \"localhost\"){" +
-						"					window.location = \"http://localhost/dr\";" +
-						"				}if(localVariabili[x] == \"cvonline\"){" +
-						"					window.location.href = \"http://cvonline.tv\";" +
-						"				}if(localVariabili[x] == \"drconsulting\"){" +
-						"					window.location.href= \"http://drconsulting.tv\";" +
-						"				}" +
-						"			}" +
-						"		}else{" +
-						"			continue;" +
-						"		}" +
-						"}" +
-						"</script>" +
-						"</body>" +
-						"</html>");
-				out.flush();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-   }
+		log.end(metodo);
+	}
 }
