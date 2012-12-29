fe535f8c95eab21190238149f5d285f738f776b1
gabriele.bertaina@gmail.com <gabriele.bertaina@gmail.com@87886287-3c3c-bf74-c0a6-4f14d648827e>
2012-12-29 15:28:23 +0000

    implementato nuova generazione codice commessa, aggiungendo l'anno. spostato il richiamo del DAO dalla jsp alla servlet

--- trunk/Azienda/src/it/azienda/dto/TipologiaCommessa.java
+++ trunk/Azienda/src/it/azienda/dto/TipologiaCommessa.java
@@ -1,10 +1,16 @@
 package it.azienda.dto;
 
 public class TipologiaCommessa {
-	
+
 	private int id_tipologia;
 	private String descrizione;
-	
+
+	public TipologiaCommessa(int id_tipologia, String descrizione) {
+		super();
+		this.id_tipologia = id_tipologia;
+		this.descrizione = descrizione;
+	}
+
 	public int getId_tipologia() {
 		return id_tipologia;
 	}
@@ -17,5 +23,4 @@ public class TipologiaCommessa {
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
-
 }
